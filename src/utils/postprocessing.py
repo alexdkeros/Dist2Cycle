d4a5eb5d99487e76a1aefa3ffbb03fd3f8e11978
Alexandros Keros <alexdkeros@gmail.com>
2021-10-31 16:14:07 +0000

    Fixed division RuntimeWarning

--- src/utils/postprocessing.py
+++ src/utils/postprocessing.py
@@ -23,8 +23,14 @@ def neighborhood_smoothing(boundarymat, logits, power=1):
     B0=boundarymat
     
     A=np.abs(B0.T@B0-np.diag(np.diag(B0.T@B0)))
-    D=np.diag(np.array(np.sum(A,axis=1)).flatten())
-    D_n=np.diag(np.diag(D)**(-1/2))
+    D=np.array(np.sum(A,axis=1)).flatten()
+    D_z=np.where(D==0)
+    D_r=D
+    D_r[D_r==0]=1
+    D_r=D_r**(-1/2)
+    D_r[D_z]=0
+    D_n=np.diag(D_r)
+    D=np.diag(D)
     Lt=D-A
     Lt=D_n@Lt@D_n
     Lt
